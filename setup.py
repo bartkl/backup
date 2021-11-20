4a13243605beb9647a7121fcb4fbaac1930a725a
Bart Kleijngeld <bartkl@gmail.com>
2021-11-20 13:47:17 +0100

    Major version bump due to breaking changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 setup(name='Backup',
       author='Bart Kleijngeld',
       author_email='bartkl@gmail.com',
-      version='1.1.0',
+      version='2.0.0',
       license='MIT',
       description='Backup script which provides an easily configurable rsync based solution.',
       url='https://github.com/bartkl/backup',
